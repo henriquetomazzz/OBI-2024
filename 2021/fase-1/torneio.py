4817d4feb7a0620d003165fafba72211f65367c3
henriquetomazzz <henriquetomaz021@gmail.com>
2024-03-13 10:45:50 -0300

    resolve questão do torneio

---
+++ 2021/fase-1/torneio.py
@@ -0,0 +1,14 @@
+count = 0
+for i in range(1, 7, 1):
+  jogo = input()
+  for x in jogo:
+    if x == "V":
+      count += 1
+if count == 5 or count == 6:
+  print("1")
+elif count == 3 or count == 4: 
+  print("2")
+elif count == 1 or count == 2: 
+  print("3")
+else: 
+  print("-1")
